6508932f965ef803ae112bc14f325844b5619eca
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2024-08-24 15:59:37 -0400

    Color Added

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -54,6 +54,102 @@ const Gallery = ({ lng }: GalleryProps) => {
           className="mt-8"
           objectFit="cover"
         />
+        <div className="mt-12 flex flex-col items-center justify-between gap-2 sm:flex-row">
+          <div className="flex basis-1/3 flex-col items-center justify-center">
+            <Image
+              src="/images/Anthracite_grey.jpg"
+              width="320"
+              height="230"
+              alt="Anthracite grey"
+              className="object-contain"
+            />
+            <p className="mt-2 text-base font-bold">Anthracite grey</p>
+          </div>
+          <div className="flex basis-1/3 flex-col items-center justify-center">
+            <Image
+              src="/images/Brushed_aluminium.jpg"
+              width="320"
+              height="230"
+              alt="Brushed aluminium"
+              className="object-contain"
+            />
+            <p className="mt-2 text-base font-bold">Brushed aluminium</p>
+          </div>
+          <div className="flex basis-1/3 flex-col items-center justify-center">
+            <Image
+              src="/images/Walnut.jpg"
+              width="320"
+              height="230"
+              alt="Walnut"
+              className="object-contain"
+            />
+            <p className="mt-2 text-base font-bold">Walnut</p>
+          </div>
+        </div>
+        <div className="mt-6 flex flex-col items-center justify-between gap-2 sm:flex-row">
+          <div className="flex basis-1/3 flex-col items-center justify-center">
+            <Image
+              src="/images/Alux_white_aluminium.jpg"
+              width="320"
+              height="230"
+              alt="Alux white aluminium"
+              className="object-contain"
+            />
+            <p className="mt-2 text-base font-bold">Alux white aluminium</p>
+          </div>
+          <div className="flex basis-1/3 flex-col items-center justify-center">
+            <Image
+              src="/images/Bronze_platinum.jpg"
+              width="320"
+              height="230"
+              alt="Bronze platinum"
+              className="object-contain"
+            />
+            <p className="mt-2 text-base font-bold">Bronze platinum</p>
+          </div>
+          <div className="flex basis-1/3 flex-col items-center justify-center">
+            <Image
+              src="/images/Dark_grey_SFTN.jpg"
+              width="320"
+              height="230"
+              alt="Dark grey SFTN"
+              className="object-contain"
+            />
+            <p className="mt-2 text-base font-bold">Dark grey SFTN</p>
+          </div>
+        </div>
+        <div className="mt-6 flex flex-col items-center justify-between gap-2 sm:flex-row">
+          <div className="flex basis-1/3 flex-col items-center justify-center">
+            <Image
+              src="/images/Sapeli.jpg"
+              width="320"
+              height="230"
+              alt="Sapeli"
+              className="object-contain"
+            />
+            <p className="mt-2 text-base font-bold">Sapeli</p>
+          </div>
+          <div className="flex basis-1/3 flex-col items-center justify-center">
+            <Image
+              src="/images/Golden-brown_oak.jpg"
+              width="320"
+              height="230"
+              alt="Golden-brown oak"
+              className="object-contain"
+            />
+            <p className="mt-2 text-base font-bold">Golden-brown oak</p>
+          </div>
+          <div className="flex basis-1/3 flex-col items-center justify-center">
+            <Image
+              src="/images/Ash_C35.jpg"
+              width="320"
+              height="230"
+              alt="Ash C35"
+              className="object-contain"
+            />
+            <p className="mt-2 text-base font-bold">Ash C35</p>
+          </div>
+        </div>
       </div>
     </>
   );
